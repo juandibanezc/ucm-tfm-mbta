342570d133ed38d122d74aaa1e0f0b2be9a71ee1
Juan David Ibanez <juandavidc7226@gmail.com>
2026-01-07 19:54:07 -0500

    Feat: Add current load ingestion module with SCD1 logic and update bronze layer parameters

---
+++ processing-data-lake/src/processing_datalake/pipelines/bronze/nodes/data_ingestion.py
@@ -0,0 +1,35 @@
+"""Module for current load ingestion."""
+
+from typing import Dict, Any
+from pyspark.sql import DataFrame
+
+from delta.tables import DeltaTable
+
+from processing_datalake.extras.utils.scd_functions import scd1_merge_delta_write
+from processing_datalake.pipelines.bronze_first_load.nodes.data_ingestion import (
+    process_table,
+)
+
+
+def ingest_current_load(
+    table: DataFrame,
+    params: Dict[str, Any],
+    target_table: DeltaTable,
+) -> None:
+    """Ingest current load data into bronze layer with SCD1 logic.
+
+    Args:
+        table (DataFrame): Input table to process.
+        params (Dict[str, Any]): Parameters for processing.
+        target_table (DeltaTable): Target delta table to write data.
+    """
+
+    source = process_table(table, params)
+
+    keys = params.get("keys")
+
+    scd1_merge_delta_write(
+        source=source,
+        target=target_table,
+        keys=keys,
+    )
