27072b8c281c95e78b44f70821607602ee61e211
Juan David Ibanez <juandavidc7226@gmail.com>
2026-01-24 14:40:26 -0500

    Refactor dataset file paths in catalog configurations and update globals for consistency

--- processing-data-lake/src/processing_datalake/__main__.py
+++ processing-data-lake/src/processing_datalake/__main__.py
@@ -1,23 +1,55 @@
 """processing-data-lake file for ensuring the package is executable
 as `processing-data-lake` and `python -m processing_datalake`
 """
-import sys
+import importlib
 from pathlib import Path
-from typing import Any
 
-from kedro.framework.cli.utils import find_run_command
+from kedro.framework.cli.utils import KedroCliError, load_entry_points
 from kedro.framework.project import configure_project
 
 
-def main(*args, **kwargs) -> Any:
+def _find_run_command(package_name):
+    try:
+        project_cli = importlib.import_module(f"{package_name}.cli")
+        # fail gracefully if cli.py does not exist
+    except ModuleNotFoundError as exc:
+        if f"{package_name}.cli" not in str(exc):
+            raise
+        plugins = load_entry_points("project")
+        run = _find_run_command_in_plugins(plugins) if plugins else None
+        if run:
+            # use run command from installed plugin if it exists
+            return run
+        # use run command from `kedro.framework.cli.project`
+        # pylint: disable=import-outside-toplevel
+        from kedro.framework.cli.project import run
+
+        return run
+    # fail badly if cli.py exists, but has no `cli` in it
+    if not hasattr(project_cli, "cli"):
+        raise KedroCliError(f"Cannot load commands from {package_name}.cli")
+    return project_cli.run
+
+
+def _find_run_command_in_plugins(plugins):
+    for group in plugins:
+        if "run" in group.commands:
+            return group.commands["run"]
+    return None
+
+
+def main(*args, **kwargs):
+    """Main entry point for running the package with `python -m processing_data_lake`."""
     package_name = Path(__file__).parent.name
     configure_project(package_name)
-
-    interactive = hasattr(sys, 'ps1')
-    kwargs["standalone_mode"] = not interactive
-
-    run = find_run_command(package_name)
-    return run(*args, **kwargs)
+    run = _find_run_command(package_name)
+    run(
+        *args,
+        **kwargs,
+        standalone_mode=False,
+        ignore_unknown_options=True,
+        allow_extra_args=True,
+    )
 
 
 if __name__ == "__main__":
