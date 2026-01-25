cf26a5e7a8d49e213086193c7ffb5381b95bf559
Juan David Ibanez <juandavidc7226@gmail.com>
2026-01-24 19:37:31 -0500

    Feat: add asynchronous extraction for MBTA API endpoints and update pipeline nodes for schedules and trips

--- processing-data-lake/src/processing_datalake/pipelines/landing/nodes/extract_mbta_api.py
+++ processing-data-lake/src/processing_datalake/pipelines/landing/nodes/extract_mbta_api.py
@@ -1,14 +1,21 @@
 """Module to create nodes for MBTA API extraction."""
 import requests as rq
+import logging
 
 from datetime import datetime
-from typing import Any, Dict
+from typing import Any, Dict, List
 from pathlib import PurePosixPath
+
+import asyncio
+from asyncio import Semaphore
+from aiohttp import ClientSession
 
 from processing_datalake.hooks import (
     get_credentials,
     get_catalog_dataset,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def last_ts() -> Dict[str, Any]:
@@ -77,5 +84,160 @@ def extract_mbta_endpoint(
         except Exception as e:
             print(f"Error fetching data from {endpoint}: {e}")
             continue
+
+    return True
+
+
+async def get_endpoint_data(
+    metadata: Dict[str, Any],
+    headers: Dict[str, str],
+    session: ClientSession,
+    semaphore: Semaphore,
+) -> bool:
+    """Asynchronously fetch data from a specific MBTA API endpoint.
+
+    Args:
+        metadata (Dict[str, Any]): Metadata for the API request.
+        headers (Dict[str, str]): Headers for the API request.
+        session (ClientSession): The aiohttp client session.
+        semaphore (Semaphore): Semaphore to limit concurrent requests.
+
+    Returns:
+        bool: The success status of the API request.
+    """
+
+    timestamp = metadata.get("timestamp")
+    target_catalog = metadata.get("catalog_dataset")
+    url_get = metadata.get("url")
+    filter_id = metadata.get("id")
+
+    year = timestamp[:4]
+    month = timestamp[4:6]
+    day = timestamp[6:8]
+
+    table_catalog = get_catalog_dataset(target_catalog)
+
+    file_path = str(table_catalog._filepath).format(
+        last_ts=timestamp,
+        year=year,
+        month=month,
+        day=day,
+        id=filter_id,
+    )
+
+    file_path_formatted = PurePosixPath(file_path)
+    table_catalog._filepath = file_path_formatted
+
+    async with semaphore:
+        async with session.get(url_get, headers=headers) as response:
+            response.raise_for_status()
+            table_catalog.save(await response.json())
+
+            return True
+
+
+async def extract_mbta_endpoint_async(
+    headers: Dict[str, str],
+    metadata_list: List[Dict[str, Any]],
+) -> bool:
+    """Asynchronously extract data from MBTA API endpoints.
+    Args:
+        url (str): Base URL for the MBTA API.
+        headers (Dict[str, str]): Headers for the API requests.
+        metadata_list (List[Dict[str, Any]]): List of metadata for each API request.
+    Returns:
+        bool: Return success status.
+    """
+
+    semaphore = Semaphore(10)
+
+    async with ClientSession() as session:
+
+        tasks = [
+            get_endpoint_data(
+                metadata,
+                headers,
+                session,
+                semaphore,
+            )
+            for metadata in metadata_list
+        ]
+
+        results = await asyncio.gather(*tasks)
+
+        return all(results)
+
+
+def extract_mbta_filter_endpoints(
+    params: Dict[str, Any],
+    last_exec: Dict[str, str],
+) -> bool:
+    """Extract data from MBTA API endpoint and save to landing zone.
+
+    Args:
+        params (Dict[str, Any]): Parameters for processing.
+        last_exec (Dict[str, str]): Last execution timestamp.
+    Returns:
+        bool: Return success status.
+    """
+
+    base_table = params.get("base_table")
+    target_catalog = params.get("target_catalog")
+    filter = params.get("filter")
+    endpoint = params.get("endpoint")
+    url = params.get("url")
+
+    credentials_name = params.get("credentials_name")
+
+    mbta_api_credentials = get_credentials(credentials_name)
+    api_key = mbta_api_credentials.get("api_key")
+
+    headers = {
+        "x-api-key": api_key,
+        "Accept": "application/json"
+    }
+
+    ids_dataset = get_catalog_dataset(base_table)
+    last_ts = last_exec.get("last_ts")
+
+    year = last_ts[:4]
+    month = last_ts[4:6]
+    day = last_ts[6:8]
+
+    file_path = str(ids_dataset._filepath).format(
+        last_ts=last_ts,
+        year=year,
+        month=month,
+        day=day,
+    )
+
+    file_path_formatted = PurePosixPath(file_path)
+    ids_dataset._filepath = file_path_formatted
+
+    dataset_id = ids_dataset.load()
+
+    logger.info("Building metadata list for asynchronous extraction.")
+
+    metadata_list = [
+        {
+            "url": url.format(
+                endpoint=endpoint,
+                filter=filter,
+                id=data['id'],
+            ),
+            "id": data['id'],
+            "timestamp": last_ts,
+            "catalog_dataset": target_catalog,
+        } for data in dataset_id['data']
+    ]
+
+    logger.info("Starting asynchronous extraction from MBTA API.")
+
+    asyncio.run(extract_mbta_endpoint_async(
+        headers,
+        metadata_list,
+    ))
+
+    logger.info("Asynchronous extraction completed.")
 
     return True
--- processing-data-lake/src/processing_datalake/pipelines/landing/pipeline.py
+++ processing-data-lake/src/processing_datalake/pipelines/landing/pipeline.py
@@ -4,6 +4,7 @@ from kedro.pipeline import Pipeline, node, pipeline
 from processing_datalake.pipelines.landing.nodes.extract_mbta_api import (
     last_ts,
     extract_mbta_endpoint,
+    extract_mbta_filter_endpoints,
 )
 
 
@@ -27,6 +28,26 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="endpoints_extraction_true",
                 name="extract_mbta_endpoints",
                 tags=["landing", "mbta"],
-            )
+            ),
+            node(
+                func=extract_mbta_filter_endpoints,
+                inputs=[
+                    "params:catalog_info_landing_schedules",
+                    "landing_last_execution@json",
+                ],
+                outputs="schedules_extraction_true",
+                name="extract_mbta_schedules",
+                tags=["landing", "mbta", "filter_endpoint"],
+            ),
+            node(
+                func=extract_mbta_filter_endpoints,
+                inputs=[
+                    "params:catalog_info_landing_trips",
+                    "landing_last_execution@json",
+                ],
+                outputs="trips_extraction_true",
+                name="extract_mbta_trips",
+                tags=["landing", "mbta", "filter_endpoint"],
+            ),
         ]
     )
