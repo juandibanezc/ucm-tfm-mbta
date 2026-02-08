cd4ee45fe17098a76268ccb7250936248eba5f80
Juan David Ibanez <juandavidc7226@gmail.com>
2026-02-07 23:01:19 -0500

    Mod: modified mbta first load tag.

--- processing-data-lake/src/processing_datalake/pipelines/bronze_first_load/pipeline.py
+++ processing-data-lake/src/processing_datalake/pipelines/bronze_first_load/pipeline.py
@@ -19,7 +19,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="bronze_routes@spark",
                 name="routes_bronze_first_load_node",
-                tags=["bronze", "first_load", "mbta"],
+                tags=["bronze", "first_load", "mbta_first_load"],
             ),
             node(
                 func=ingest_dimensions,
@@ -29,7 +29,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="bronze_route_patterns@spark",
                 name="route_pattern_bronze_first_load_node",
-                tags=["bronze", "first_load", "mbta"],
+                tags=["bronze", "first_load", "mbta_first_load"],
             ),
             node(
                 func=ingest_dimensions,
@@ -39,7 +39,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="bronze_stops@spark",
                 name="stop_bronze_first_load_node",
-                tags=["bronze", "first_load", "mbta"],
+                tags=["bronze", "first_load", "mbta_first_load"],
             ),
             node(
                 func=ingest_dimensions,
@@ -49,7 +49,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="bronze_schedules@spark",
                 name="schedules_bronze_first_load_node",
-                tags=["bronze", "first_load", "mbta"],
+                tags=["bronze", "first_load", "mbta_first_load"],
             ),
             node(
                 func=ingest_dimensions,
@@ -59,7 +59,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="bronze_trips@spark",
                 name="trips_bronze_first_load_node",
-                tags=["bronze", "first_load", "mbta"],
+                tags=["bronze", "first_load", "mbta_first_load"],
             ),
             node(
                 func=ingest_dimensions,
@@ -69,7 +69,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="bronze_points@spark",
                 name="points_bronze_first_load_node",
-                tags=["bronze", "first_load", "nws"],
+                tags=["bronze", "first_load", "nws_first_load"],
             ),
             node(
                 func=ingest_dimensions,
@@ -79,7 +79,7 @@ def create_pipeline(**kwargs) -> Pipeline:
                 ],
                 outputs="bronze_grids@spark",
                 name="grids_bronze_first_load_node",
-                tags=["bronze", "first_load", "nws"],
+                tags=["bronze", "first_load", "nws_first_load"],
             ),
         ]
     )
