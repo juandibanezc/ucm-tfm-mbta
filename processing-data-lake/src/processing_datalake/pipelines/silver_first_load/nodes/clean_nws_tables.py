2d28ae84aebf65b9621ba973817ee3d56226974d
Juan David Ibanez <juandavidc7226@gmail.com>
2026-02-08 22:50:26 -0500

    Feat: added new cleaning module to silver tables.

---
+++ processing-data-lake/src/processing_datalake/pipelines/silver_first_load/nodes/clean_nws_tables.py
@@ -0,0 +1,87 @@
+"""Module to clean NWS tables in the silver layer."""
+import pyspark.sql.functions as F
+from pyspark.sql import DataFrame
+
+from processing_datalake.extras.utils.scd_functions import audit_cols
+
+
+def transform_grids_table(
+    source: DataFrame,
+) -> DataFrame:
+    """Transform NWS points table.
+
+    Args:
+        source (DataFrame): Input DataFrame to be transformed.
+    Returns:
+        DataFrame: Transformed DataFrame.
+
+    """
+
+    table_df = source.select(
+        F.col("grid_id"),
+        F.col("elevation.unitCode").alias("elevation_unit"),
+        F.col("elevation.value").alias("elevation_value"),
+        F.col("forecast_generator"),
+        F.col("generated_at"),
+        F.explode_outer(F.col("periods")).alias("period_forecast"),
+        F.col("units"),
+        F.col("update_time"),
+    )
+
+    return table_df
+
+
+def transform_point_table(
+    source: DataFrame,
+) -> DataFrame:
+    """Transform NWS point table.
+
+    Args:
+        source (DataFrame): Input DataFrame to be transformed.
+    Returns:
+        DataFrame: Transformed DataFrame.
+    """
+
+    table_df = source.select(
+        F.col("stop_id"),
+        F.col("grid_box"),
+        F.col("grid_x").cast("integer").alias("grid_x"),
+        F.col("grid_y").cast("integer").alias("grid_y"),
+        F.concat_ws("_", F.col("grid_x"), F.col("grid_y")).alias("grid_id"),
+    )
+
+    return table_df
+
+
+def clean_grids_table(
+    table: DataFrame,
+) -> DataFrame:
+    """Clean NWS grids table.
+
+    Args:
+        table (DataFrame): Input DataFrame to be cleaned.
+    Returns:
+        DataFrame: Cleaned DataFrame.
+
+    """
+
+    table_df = transform_grids_table(table)
+
+    return audit_cols(table_df)
+
+
+def clean_points_table(
+    table: DataFrame,
+) -> DataFrame:
+    """Clean NWS points table.
+
+    Args:
+        table (DataFrame): Input DataFrame to be cleaned.
+    Returns:
+        DataFrame: Cleaned DataFrame.
+
+    """
+
+    table_df = transform_point_table(table)
+
+    return audit_cols(table_df)
